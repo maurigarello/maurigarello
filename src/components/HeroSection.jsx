35cde563d9410175548aae4ca967810b372b97a9
Mauricio Garello <mauriciogarellog2g@gmail.com>
2022-11-28 20:26:14 -0300

    upgrade

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function HeroSection() {
+	return (
+		<div id="hero" className="flex items-center justify-center flex-col py-20">
+			<div className="text-center">
+				<h1 className="text-2xl md:text-4xl mb-1 md:mb-3 text-indigo-600 dark:text-indigo-500 font-semibold">
+					Hi, This is Mauricio Garello
+				</h1>
+				<p className="text-md md:text-xl max-w-md mb-3 text-gray-600 dark:text-gray-300">
+					 
+					 
+				</p>
+				
+			</div>
+		</div>
+	);
+}
+
+export default HeroSection;
